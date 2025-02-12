8cd44ee493b7e7fcd17cb937cb096653a0d06fb9
KAZ CHIONG 2304032B <2304032B@student.tp.edu.sg>
2025-02-12 21:25:27 +0800

    added custom identity logic

--- Nutrify/Program.cs
+++ Nutrify/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<NutrifyUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<NutrifyContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
